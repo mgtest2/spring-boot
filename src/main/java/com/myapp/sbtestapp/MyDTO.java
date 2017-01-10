f031c3fad118d1d0ee3bb7f13415e040f50123fe
mgtest2 <test@test.com>
2017-01-10 15:50:31 +0000

    ttt

---
+++ src/main/java/com/myapp/sbtestapp/MyDTO.java
@@ -0,0 +1,17 @@
+package com.myapp.sbtestapp;
+
+public class MyDTO {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
