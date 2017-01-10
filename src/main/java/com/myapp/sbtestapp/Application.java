4dc2cfc1905f77ee902370a5f3aff6d764754a0e
guptam2 <madhur.gupta@theaa.com>
2017-01-10 14:50:01 +0000

    spring boot

---
+++ src/main/java/com/myapp/sbtestapp/Application.java
@@ -0,0 +1,16 @@
+package com.myapp.sbtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Application.class, args);
+		
+	}
+	
+	
+}
